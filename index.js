929bb8771b469dfb5b4255ec18cf016dfb391ebc
KaziShohiduzzaman <kazishohiduzzaman786@gmail.com>
2021-11-07 19:49:25 +0600

    fixed some issue

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const app = express()
 const cors = require('cors');
 require('dotenv').config()
-const port = process.env.PORT || 5000;
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express()
+const port = process.env.PORT || 5000;
+
 
 
 //middleWare
